c203eac8ada4a34d05b3fdc9f577880e5a87075a
Max Persson Andersen <121873346+maxpers@users.noreply.github.com>
2023-02-22 14:55:56 +0100

    Update flutter_highlight.dart
    Make highlighted text selectable

--- flutter_highlight/lib/flutter_highlight.dart
+++ flutter_highlight/lib/flutter_highlight.dart
@@ -90,7 +90,7 @@ class HighlightView extends StatelessWidget {
     return Container(
       color: theme[_rootKey]?.backgroundColor ?? _defaultBackgroundColor,
       padding: padding,
-      child: RichText(
+      child: SelectableText.rich(
         text: TextSpan(
           style: _textStyle,
           children: _convert(highlight.parse(source, language: language).nodes!),
